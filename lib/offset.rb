1bcd526d45c7109b2cbe126b7ff33de46250000f
Paul Debevec <paulmdebevec@gmail.com>
2019-11-02 14:47:57 -0600

    Added date_key_offsets method

--- lib/offset.rb
+++ lib/offset.rb
@@ -8,4 +8,10 @@ class Offset
   def transmission_date
     Time.now.strftime("%d%m%y")
   end
+
+  def date_key_offsets
+    squared_date = (@date.to_i ** 2).to_s # => 2161
+    squared_date[-4..-1]
+  end
+
 end
