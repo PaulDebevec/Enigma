26213681da80416d7871766b0af9ad12d3a9ba84
Paul Debevec <paulmdebevec@gmail.com>
2019-11-02 14:24:58 -0600

    Added test_it_initializes

--- test/key_test.rb
+++ test/key_test.rb
@@ -6,4 +6,13 @@ class KeyTest < Minitest::Test
     key = Key.new("42563")
     assert_instance_of Key, key
   end
+
+  def test_it_initializes
+    key = Key.new("42563")
+    assert_equal "42563", key.number_key
+    assert_equal "42", key.a
+    assert_equal "25", key.b
+    assert_equal "56", key.c
+    assert_equal "63", key.d
+  end
 end
