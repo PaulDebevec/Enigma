3cf67fe095f12463fc6a9f88ae4b1a7fad7a993e
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 12:11:15 -0700

    Added test_it_initializes

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -7,4 +7,11 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
   end
 
+  def test_initialize
+    shift = Shift.new("42563", "021119")
+    assert_equal 44, shift.a
+    assert_equal 26, shift.b
+    assert_equal 62, shift.c
+    assert_equal 64, shift.d
+  end
 end
