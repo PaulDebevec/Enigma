0885e802c85fedac16a6028d88598319d2e249b5
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 12:17:03 -0700

    Added get_offset method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,4 +4,11 @@ class Enigma
   def initialize
     @character_set = ("a".."z").to_a << " "
   end
+  def get_offset(shift, index)
+    remainder = index % 4
+    return shift.a if remainder == 0
+    return shift.b if remainder == 1
+    return shift.c if remainder == 2
+    return shift.d if remainder == 3
+  end
 end
