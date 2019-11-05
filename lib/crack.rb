939da7ad8bb72124db19f156909d24e1a1674d19
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 12:28:55 -0700

    Added Crack runner file

---
+++ lib/crack.rb
@@ -0,0 +1,12 @@
+require './lib/enigma'
+
+input_file = ARGV[0]
+output_file = ARGV[1]
+date = ARGV[2]
+
+message_to_crack = File.read(input_file)
+enigma = Enigma.new
+decryption = enigma.crack(message_to_crack, date)
+File.open(output_file, "w") {|f| f.write(decryption[:decryption]) }
+
+p "Created '#{output_file}' with the key #{decryption[:key]} and date #{decryption[:date]}"
