610f0decdd629c1bcfac3205cfe2108d82cf1eeb
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 12:25:37 -0700

    Refacroted Key class initialize

--- lib/key.rb
+++ lib/key.rb
@@ -1,12 +1,13 @@
 class Key
+  #can we remove number_key from attr_reader?
   attr_reader :number_key, :a, :b, :c, :d
 
   def initialize(number_key = nil)
     @number_key = number_key.nil? ? random_key : number_key
-    @a = @number_key[0..1]
-    @b = @number_key[1..2]
-    @c = @number_key[2..3]
-    @d = @number_key[3..4]
+    @a = @number_key[0..1].to_i
+    @b = @number_key[1..2].to_i
+    @c = @number_key[2..3].to_i
+    @d = @number_key[3..4].to_i
   end
 
   def random_key
