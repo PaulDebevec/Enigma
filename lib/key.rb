c1dd14259e80936cc8870616c0ffc78708f4aa89
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 20:40:22 -0700

    Changed variable name from rangom_key to random_key_generator

--- lib/key.rb
+++ lib/key.rb
@@ -3,14 +3,14 @@ class Key
   attr_reader :number_key, :a, :b, :c, :d
 
   def initialize(number_key = nil)
-    @number_key = number_key.nil? ? random_key : number_key
+    @number_key = number_key.nil? ? random_key_generator : number_key
     @a = @number_key[0..1].to_i
     @b = @number_key[1..2].to_i
     @c = @number_key[2..3].to_i
     @d = @number_key[3..4].to_i
   end
 
-  def random_key
+  def random_key_generator
     rand.to_s[2..6]
   end
 end
