259424f50171c2bd949385a54894717e3c159e44
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 21:19:20 -0700

    Added comments to the shift class stating the purpose

--- lib/shift.rb
+++ lib/shift.rb
@@ -4,6 +4,7 @@ require './lib/offset'
 class Shift
   attr_reader :a, :b, :c, :d, :key, :offset
 
+  # Initializes both key and offset and adds them together to get the full shift
   def initialize(key, date)
     @key = Key.new(key)
     @offset = Offset.new(date)
