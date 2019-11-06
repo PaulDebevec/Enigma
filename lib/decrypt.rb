60cc558d078f1ebdba121cb62247d8ded644a958
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 20:35:38 -0700

    Changed unclear variable name from f to file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -8,6 +8,6 @@ date_key = ARGV[3]
 message_to_decrypt = File.read(file_to_decrypt)
 enigma = Enigma.new
 decryption = enigma.decrypt(message_to_decrypt, decryption_key, date_key)
-File.open(decrypted_file, "w") {|f| f.write(decryption[:decryption]) }
+File.open(decrypted_file, "w") {|file| file.write(decryption[:decryption]) }
 
 p "Created '#{decrypted_file}' with the key #{decryption[:key]} and date #{decryption[:date]}"
